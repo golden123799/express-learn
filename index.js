f457d0ef3fdab57c94c6ffcfbe54f9b5fd05f761
Golden Fam <goldenfamfc@gmail.com>
2023-07-05 10:15:00 +0800

    install mongoose and trying to connect to mongodb

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const articleRouter = require('./routes/articles');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://golden_user:S3fvsQOxIjsUV90P@cluster0.jayp3uh.mongodb.net/blog", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })  
+  .then(() => {
+    console.log('Connected to MongoDB');
+
+  })
+  .catch(err => console.error('Error connecting to MongoDB:', err));
 
 app.set("view engine", "ejs");
 
